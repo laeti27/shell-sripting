61ce2c908938d3c2e4c2bf2c46bf85d498f665c4
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-06 22:30:28 -0600

    AutoPush

--- 03-color.sh
+++ 03-color.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-#Each and every color you see on terminal will have a color code and we need to use that code based on our need
+#printing or presentingh your work in a nice neatly way using colors 
+#colors       foreground          background
+#red              31                   41
+#green            32                    42
+#yellow           33                    43
+#blue             34                    44
+#Magenta          36                    45
 
-# Colors          Foreground           Background
-# Red                31                  41
-# Green              32                  42
-# Yellow             33                  43
-# Blue               34                  43
-# Cyan               35                  45
-# Magenta            36                  45
+# syntax to print COLORS is : 
 
-# Syntax to Print COLORS is :
+# echo -e  "\e[COLORCODEm I am printing Red color \e[0m"
 
-# echo -e "\e[COLORCODEm I am printing Red color \e[0m"
-
-echo -e "\e31m I am printing Red color \e[0m"
-echo -e "\e32m I am printing Green color \e[0m"
-echo -e "\e33m I am printing Yellow color \e[0m"
-echo -e "\e34m I am printing Blue color \e[0m"
-echo -e "\e35m I am printing Cyan color \e[0m"
+echo -e "\e[31m I am printing Red color \e[0m"
+echo -e "\e[32m I am printing Green color \e[0m"
+echo -e "\e[33m I am printing  Yellow color \e[0m"
+echo -e "\e[34m I am printing Blue color \e[0m"
+echo -e "\e[35m I am printing Magenta color \e[0m"
