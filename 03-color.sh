6ad68d290bdc8dd03a511536e0d07fd122000783
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-06 22:17:09 -0600

    AutoPush

--- 03-color.sh
+++ 03-color.sh
@@ -7,5 +7,15 @@
 # Green              32                  42
 # Yellow             33                  43
 # Blue               34                  43
-# Magenta            35                  45
-# Cyan               36                  46
+# Cyan               35                  45
+# Magenta            36                  45
+
+# Syntax to Print COLORS is :
+
+# echo -e "\e[COLORCODEm I am printing Red color \e[0m"
+
+echo -e "\e31m I am printing Red color \e[0m"
+echo -e "\e32m I am printing Green color \e[0m"
+echo -e "\e33m I am printing Yellow color \e[0m"
+echo -e "\e34m I am printing Blue color \e[0m"
+echo -e "\e35m I am printing Cyan color \e[0m"
