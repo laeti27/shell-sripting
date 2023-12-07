e3e1ca7715ade2659a23f17f9f2d116d82f31bf8
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-06 21:06:57 -0600

    new3

--- 02-esc-sequence.sh
+++ 02-esc-sequence.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+# Escape sequence characters
+
+# \n : New Line
+# \t : tab Space
 
 echo "hi"
-echo "line1\nline2"
-echo line3
-echo line4
+echo -e "line1\nline2"
+echo -e "line3\tline4"
+echo line5
