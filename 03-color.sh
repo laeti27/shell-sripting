9b22a1b9aea0e46e1adbf592aa75b80179018306
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-06 22:00:27 -0600

    AutoPush

---
+++ 03-color.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Each and every color you see on terminal will have a color code and we need to use that code based on our need
+
+# Colors          Foreground           Background
+# Red                31                  41
+# Green              32                  42
+# Yellow             33                  43
+# Blue               34                  43
+# Magenta            35                  45
+# Cyan               36                  46
