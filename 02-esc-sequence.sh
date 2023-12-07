cf9a9345664967d5eb0bbe28a505a22f9c1c0600
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-06 21:52:40 -0600

    AutoPush

--- 02-esc-sequence.sh
+++ 02-esc-sequence.sh
@@ -8,3 +8,7 @@ echo "hi"
 echo -e "line1\nline2"
 echo -e "line3\tline4"
 echo line5
+
+# Whenever uou are using special characters, always enclose-them in DOUBLE-Quotes
+# ""   : Double Quotes
+# '.'  : Single Quotes
