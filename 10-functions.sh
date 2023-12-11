4513bc341ad9a9be0b5a6a4d3df5e7ffdabc2df3
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-11 14:33:12 -0600

    AutoPush

---
+++ 10-functions.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# There are 4 types of command available :
+
+# 1) Binary                       ( /bin  , /sbin )
+# 2) Aliases                      ( Alises are shortcuts, alias net="netstat -tulpn" )
+# 3) Shell Built-in Commands
+# 4) Functions                    # Functions are nothing but a set of command that can be wrtten in a sequence and can be called n num
+
+
+
+# How to declare a function ?
+
+# f() {
+#     echo hai
+# }
+
+# How to you call a function
+
+f  # calling a function
+
+# sample() {
+#       echo Hai from sample function
+#       echo sample function is completed
+# }
+
+
+# sample
+# echo sample call is completed
+# sample
+
+
+
+b56() {
+    echo "This is our batch56 function"
+    echo "we are learning fuctions"
+    echo "Today date is:"
+    date
+    echo "fuction b56 is completed"
+}
+
+b56 
