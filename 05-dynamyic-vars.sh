5f49279f39c436a3f3a5cd577c50871c29da4048
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-11 13:10:00 -0600

    AutoPush

--- 05-dynamyic-vars.sh
+++ 05-dynamyic-vars.sh
@@ -3,5 +3,5 @@
 # DATE="11DECEMBER2023"
 DATE=$(date +%F)
 NO_OF_SESSIONS=$(who|wc -l)
-echo -e "Good Morning , Todays date is \e[32m $DATE \e[0m"
-echo -e "Total Number Of Connected Sessions : \e[32m $NO_OF_SESSIONS \e[0m"
+echo -e "Good Morning , Todays date is \e[33m $DATE \e[0m"
+echo -e "Total Number Of Connected Sessions : \e[33m $NO_OF_SESSIONS \e[0m"
