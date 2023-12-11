ed6a4a86a1de78319b40cfa1506edb76be92af50
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-11 12:27:57 -0600

    AutoPush

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-#printing or presentingh your work in a nice neatly way using colors 
-#colors       foreground          background
-#red              31                   41
-#green            32                    42
-#yellow           33                    43
-#blue             34                    44
-#Magenta          36                    45
+# What is a variable ?
+# A variable is something that holds data dynamically
 
-# syntax to print COLORS is : 
 
-# echo -e  "\e[COLORCODEm I am printing Red color \e[0m"
+#the advantage & disadvantage in shell is that there are no Data Types
 
-echo -e "\e[31m I am printing Red color \e[0m"
-echo -e "\e[32m I am printing Green color \e[0m"
-echo -e "\e[33m I am printing  Yellow color \e[0m"
-echo -e "\e[34m I am printing Blue color \e[0m"
-echo -e "\e[35m I am printing Magenta color \e[0m"
+a=10
+b=abc
+# In bash, everything will be considered as tring by defauldt.
 
-# Background + Foregroung
-echo -e "\e[43;34m I am printing background +forground \e[0m"
+#How can we print a variable ? $var
+
+echo $a 
+echo "printing the value of a $a"
+echo "printing the value of a ${a}"
+
+echo printing the value of b ${b}
+
+echo ...
