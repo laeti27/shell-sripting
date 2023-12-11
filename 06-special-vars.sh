92a5bf32851c18af9652388f8c425401cbce0e57
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-11 13:23:09 -0600

    AutoPush

--- 06-special-vars.sh
+++ 06-special-vars.sh
@@ -3,3 +3,8 @@
 # Special Variables are $0 to $9 , $* , $@ , $#, $$
 
 echo "printing Script Name : $0"
+
+echo "First Argument is : $1 "
+echo "Second Argument is : $2 "
+#bash scriptingName.sh firstArg  secondArg  thirdArg
+#                         $1         $2        $
