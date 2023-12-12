15af6279b8461c6df491ab5a9d11c9ebe79dc7eb
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-11 23:25:37 -0600

    AutoPush

--- roboshop/wrapper.sh
+++ roboshop/wrapper.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-bash components/frontend.sh
+bash components/$1.sh
+
+#if [ $? -ne 0 ] ; then
+#     echo -e "e[31m example usage: \e[om bash wrapper.sh componentName"
+#     exit 30
+#fi 
