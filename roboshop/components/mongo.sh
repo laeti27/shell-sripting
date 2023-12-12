56b594977f3c842594ba67e0eb55bff31b289e23
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-12 14:09:09 -0600

    Auto Push

--- roboshop/components/mongo.sh
+++ roboshop/components/mongo.sh
@@ -28,3 +28,10 @@ stat $?
 echo -n "Installing $COMPONENT :"
 yum install -y mongodb-org &>> ${LOGFILE} 
 stat $?
+
+echo -n "starting $COMPONENT :"
+
+systemctl enable mongod     &>> $LOGFILE
+systemctl daemon-reload    &>> $LOGFILE
+systemctl start mongod    &>> $LOGFILE
+stat $?
