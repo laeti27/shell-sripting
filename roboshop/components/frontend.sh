1d1461014b7312982b28f3dce413d6b21b2fe233
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-12 00:02:22 -0600

    AutoPush

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 USER_ID=$(id -u)
+COMPONENT=$1
 
 if [ $USER_ID -ne 0 ] ; then
    echo -e "\e[31m This script is expected to be executed with sudo or as a root user \e[0m"
@@ -18,6 +19,13 @@ else
    echo -e "\e[31m failure \e[0m"   
 fi
 
+echo -n "Downloading component $1 :"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+if [ $? -eq 0 ] ; then
+   echo -e "\e[32m success \e[0m"
+else 
+   echo -e "\e[31m failure \e[0m"   
+fi
 
 
 # systemctl enable nginx
