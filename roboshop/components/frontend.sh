48730028998bf39f488aa8d8f57eed1ba836254f
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-12 12:12:59 -0600

    Python Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -3,69 +3,54 @@
 USER_ID=$(id -u)
 COMPONENT=$1
 
+stat() {
+   if [ $1 -eq 0 ] ; then 
+       echo -e "\e[32m Success \e[0m"
+   else 
+       echo -e "\e[31m Failure \e[0m"
+   fi 
+}
+
 if [ $USER_ID -ne 0 ] ; then
    echo -e "\e[31m This script is expected to be executed with sudo or as a root user \e[0m"
    echo -e "\e[35m Example Usage: \n\t\t \e[0m sudo bash scriptName componentName"
    exit 1
 fi
 
-echo -e"***** \e[35m configuring frontend \e[0m *****"
+echo -e"***** \e[35m configuring $COMPONENT \e[0m *****"
 
 echo -n "Install Nginx :"
 yum install nginx -y      &>> /tmp/frontend.log
-if [ $? -eq 0 ] ; then
-   echo -e "\e[32m success \e[0m"
-else 
-   echo -e "\e[31m failure \e[0m"   
-fi
+stat $?
 
 echo -n "Downloading component $COMPONENT :"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-if [ $? -eq 0 ] ; then
-   echo -e "\e[32m success \e[0m"
-else 
-   echo -e "\e[31m failure \e[0m"   
-fi
+stat $?
 
 echo -n "Cleanup of $COMPONENT component : "
 cd /usr/share/nginx/html
 rm -rf *          &>> /tmp/frontend.log
-if [ $? -eq 0 ] ; then
-   echo -e "\e[32m success \e[0m"
-else 
-   echo -e "\e[31m failure \e[0m"   
-fi
+stat $?
 
 echo -n "Extrating $COMPONENT: "
 unzip /tmp/frontend.zip     &>> /tmp/frontend.log
-if [ $? -eq 0 ] ; then
-   echo -e "\e[32m success \e[0m"
-else 
-   echo -e "\e[31m failure \e[0m"   
-fi
+stat $?
 
 echo -n "Configuring $COMPONENT: "
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
-
-if [ $? -eq 0 ] ; then
-   echo -e "\e[32m success \e[0m"
-else 
-   echo -e "\e[31m failure \e[0m"   
-fi
+stat $?
 
 echo -n "Restarting $COMPONENT: "
 systemctl enable nginx     &>> /tmp/frontend.log
 systemctl daemon-reload    &>> /tmp/frontend.log
 systemctl restart nginx    &>> /tmp/frontend.log
+stat $?
 
-if [ $? -eq 0 ] ; then
-   echo -e "\e[32m success \e[0m"
-else 
-   echo -e "\e[31m failure \e[0m"   
-fi  
+echo -e"***** \e[35m configuring $COMPONENT \e[0m *****"
+
 
 
  
