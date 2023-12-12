7bf900808c0a4c33fa041567f96497f2c8b90a6f
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-12 14:30:16 -0600

    Auto Push

--- roboshop/components/mongo.sh
+++ roboshop/components/mongo.sh
@@ -39,3 +39,10 @@ stat $?
 echo -n "enabling $COMPONENT visibility :"
 sed -i -e 's/127.0.0.1/0.0.0.0/' mongod.conf
 stat $?
+
+echo -n "starting $COMPONENT :"
+
+systemctl enable mongod     &>> $LOGFILE
+systemctl daemon-reload    &>> $LOGFILE
+systemctl start mongod    &>> $LOGFILE
+stat $?
