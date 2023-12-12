6b6a2afc6e1a918e2b73c63550417fec85496294
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-11 22:54:18 -0600

    AutoPush

---
+++ 13-operators.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+<<COMMENT
+
+### Expressions are categorized in to three types and based on the expression, we need to use operators
+
+    1. Numbers
+    2. Strings
+    3. Files
+
+Operators on numbers:
+
+    -eq , -ne , -gt, -ge. -lt, -le
+
+    [ 1 -eq 1 ]
+    [ 1 -ne 1 ]
+
+Opreatiors on strings:
+
+
+    = , == ,!=
+
+    [ abc = abs ]
+
+    -z , -n
