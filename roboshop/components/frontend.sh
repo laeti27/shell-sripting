62cf9bf399f6b7b3740cce88f7f81d188aa34e52
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-11 23:17:59 -0600

    AutoPush

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-echo "configuring frontend"
+echo -e"***** \e[35m configuring frontend \e[0m *****"
+
+echo "install Nginx :"
+yum install nginx -y
+# systemctl enable nginx
+# systemctl start nginx
+# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-main README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+
+ 
+ 
