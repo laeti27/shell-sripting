0ccdf031297f852f65c391fee0df6194be4ca97e
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-12 11:42:19 -0600

    AutoPush

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -27,17 +27,43 @@ else
    echo -e "\e[31m failure \e[0m"   
 fi
 
+echo -n "Cleanup of $1 component : "
+cd /usr/share/nginx/html
+rm -rf *          &>> /tmp/frontend.log
+if [ $? -eq 0 ] ; then
+   echo -e "\e[32m success \e[0m"
+else 
+   echo -e "\e[31m failure \e[0m"   
+fi
 
-# systemctl enable nginx
-# systemctl start nginx
-# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-# cd /usr/share/nginx/html
-# rm -rf *
-# unzip /tmp/frontend.zip
-# mv frontend-main/* .
-# mv static/* .
-# rm -rf frontend-main README.md
-# mv localhost.conf /etc/nginx/default.d/roboshop.conf
+echo -n "Extrating $1: "
+unzip /tmp/frontend.zip     &>> /tmp/frontend.log
+if [ $? -eq 0 ] ; then
+   echo -e "\e[32m success \e[0m"
+else 
+   echo -e "\e[31m failure \e[0m"   
+fi
+
+echo -n "Configuring $1: "
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+if [ $? -eq 0 ] ; then
+   echo -e "\e[32m success \e[0m"
+else 
+   echo -e "\e[31m failure \e[0m"   
+fi
+
+echo -n "Restarting $1: "
+systemctl enable nginx     &>> /tmp/frontend.log
+systemctl daemon-reload    &>> /tmp/frontend.log
+systemctl restart nginx    &>> /tmp/frontend.log
+if [ $? -eq 0 ] ; then
+   echo -e "\e[32m success \e[0m"
+else 
+   echo -e "\e[31m failure \e[0m"   
+fi
 
 
  
