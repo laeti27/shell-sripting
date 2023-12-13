789ee40c8427887a18aaa495968c1a15ab7a2606
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-13 14:38:02 -0600

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -44,7 +44,7 @@ stat $?
 
 
 echo -n "Updating Reverse Proxy :"
-sed -i -e "/${COMPONENT}/s/localhost/${COMPONENT}.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
+sed -i -e "/catalogue/s/localhost/catalogue.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
 stat $?
 
 echo -n "Restarting $COMPONENT: "
