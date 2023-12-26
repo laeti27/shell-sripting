a681897a45f94875a7feef826851a443325878fe
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-26 10:35:08 -0600

    Auto Push

---
+++ roboshop/components/mysql.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+COMPONENT=mysql.sh
+MONGO_REPO="https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo"
+SCHEMA_URL="https://github.com/stans-robot-project/mongodb/archive/main.zip"
+
+source components/common.sh
+
---
+++ roboshop/create-ec2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This script is going to create a servers
+
+AMI_ID="ami-0f75a13ad2e340a58" # Hardcoding is a bas-choice particulary with AMI-ID as it's going to be chnged when you register a new AMI.
+SGID="sg-078189db2e4e1578f"
